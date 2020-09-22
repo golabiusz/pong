cec7d5828a7d99ad14ab2e4d2a793eafc008a793
golabiusz <golabiusz@gmail.com>
2020-09-22 21:13:07 +0200

    Collisions and sounds (#1)

--- app/src/main/java/com/golabiusz/pong/Ball.java
+++ app/src/main/java/com/golabiusz/pong/Ball.java
@@ -1,4 +1,87 @@
 package com.golabiusz.pong;
 
-class Ball {
+import android.graphics.RectF;
+
+class Ball
+{
+    private RectF rect;
+    private float width;
+    private float height;
+    private float xVelocity;
+    private float yVelocity;
+
+    Ball(int screenWidth)
+    {
+        width = screenWidth / 100;
+        height = screenWidth / 100;
+
+        rect = new RectF();
+    }
+
+    public RectF getRect()
+    {
+        return rect;
+    }
+
+    public void reset(int screenWidth, int screenHeight)
+    {
+        rect.left = screenWidth / 2;
+        rect.top = 0;
+        rect.right = rect.left + width;
+        rect.bottom = height;
+
+        yVelocity = -(screenHeight / 3);
+        xVelocity = (screenHeight / 3);
+    }
+
+    public void updatePosition(long fps)
+    {
+        rect.left = rect.left + (xVelocity / fps);
+        rect.top = rect.top + (yVelocity / fps);
+
+        rect.right = rect.left + width;
+        rect.bottom = rect.top + height;
+    }
+
+    public void increaseVelocity()
+    {
+        // increase the speed by 10%
+        xVelocity = xVelocity * 1.1f;
+        yVelocity = yVelocity * 1.1f;
+    }
+
+    public void reverseYVelocity()
+    {
+        yVelocity = -yVelocity;
+    }
+
+    public void reverseXVelocity()
+    {
+        xVelocity = -xVelocity;
+    }
+
+    public void batBounce(RectF batPosition)
+    {
+        float batCenter = batPosition.left + (batPosition.width() / 2);
+        float ballCenter = rect.left + (width / 2);
+
+        float relativeIntersect = (batCenter - ballCenter);
+        if (relativeIntersect < 0) {
+            goRight();
+        } else {
+            goLeft();
+        }
+
+        reverseYVelocity();
+    }
+
+    private void goRight()
+    {
+        xVelocity = Math.abs(xVelocity);
+    }
+
+    private void goLeft()
+    {
+        xVelocity = -Math.abs(xVelocity);
+    }
 }
--- app/src/main/java/com/golabiusz/pong/Bat.java
+++ app/src/main/java/com/golabiusz/pong/Bat.java
@@ -1,4 +1,69 @@
 package com.golabiusz.pong;
 
-class Bat {
+import android.graphics.RectF;
+
+class Bat
+{
+    private RectF rect;
+    private float length;
+    private float height;
+    private float speed;
+
+    final int STOPPED = 0;
+    final int LEFT = 1;
+    final int RIGHT = 2;
+
+    private int movementState = STOPPED;
+
+    Bat(int screenWidth, int screenHeight)
+    {
+        length = screenWidth / 8;
+        height = screenHeight / 40;
+
+        rect = new RectF();
+    }
+
+    public RectF getRect()
+    {
+        return rect;
+    }
+
+    public void reset(int screenWidth, int screenHeight)
+    {
+        // Configure the starting location of the bat - the middle horizontally, the bottom vertically
+        rect.left = screenWidth / 2 - length / 2;
+        rect.top = screenHeight - height;
+        rect.right = rect.left + length;
+        rect.bottom = screenHeight;
+
+        // Configure the speed of the bat - can cover the width of the screen in 1 second
+        speed = screenWidth;
+    }
+
+    public void setMovementState(int state)
+    {
+        movementState = state;
+    }
+
+    public void updatePosition(long fps, int screenWidth)
+    {
+        float xCoord = rect.left;
+
+        if (movementState == LEFT) {
+            xCoord = xCoord - speed / fps;
+        }
+
+        if (movementState == RIGHT) {
+            xCoord = xCoord + speed / fps;
+        }
+
+        if (xCoord < 0) {
+            xCoord = 0;
+        } else if (xCoord + length > screenWidth) {
+            xCoord = screenWidth - length;
+        }
+
+        rect.left = xCoord;
+        rect.right = xCoord + length;
+    }
 }
